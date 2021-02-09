afdf808818fa25a50895ceb23d4b1fee2f76728e
NilsGamer <78762946+NilsGamer@users.noreply.github.com>
2021-02-09 01:07:36 +0100

    Update 1.11

--- globalbot.py
+++ globalbot.py
@@ -101,4 +101,4 @@ def get_globalChat_id(guild_id):
 
 ###########################################################
 
-client.run("NzAyMTQwNzkyODQ3ODU5NzEz.Xp7tkg.WnbyL2mpM5YpJOu66bNZ-eFD4UE")
+bot.run("NzAyMTQwNzkyODQ3ODU5NzEz.Xp7tkg.WnbyL2mpM5YpJOu66bNZ-eFD4UE")
